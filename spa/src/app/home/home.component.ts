c7dccc40dc4a1536be31977a8d925586b61ff21a
user16 <user16@PC3>
2021-04-13 01:48:59 +0300

    styles

--- spa/src/app/home/home.component.ts
+++ spa/src/app/home/home.component.ts
@@ -11,5 +11,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  public started(): boolean {
+    return window.history && window.history.length > 1 && location.href.indexOf(document.referrer) > -1;
+  }
 }
