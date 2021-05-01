a8373c3dd10754fb0a5155ed19b07971f4868b6a
user16 <user16@PC3>
2021-05-02 02:14:33 +0300

    share changed to 1400-2100

--- spa/src/app/share/provider-info.ts
+++ spa/src/app/share/provider-info.ts
@@ -1,4 +1,5 @@
 export class ProviderInfo {
-    public url!: string;
-    public name!: string;
+  public url!: string;
+  public name!: string;
+  public description!: string;
 }
--- spa/src/app/share/share.component.ts
+++ spa/src/app/share/share.component.ts
@@ -26,7 +26,11 @@ export class ShareComponent {
         urlTransformer.appendParameter('postTitle', title);
         urlTransformer.appendParameter('postImg', image);
         urlTransformer.appendParameter('postDesc', description);
-        return { name: item.name, url: urlTransformer.resultUrl };
+        return {
+          name: item.name,
+          url: urlTransformer.resultUrl,
+          description: `${item.description} will be sent to ${item.name}`
+        };
       });
     } else {
       this.transformedLinks = [];
@@ -41,9 +45,8 @@ export class ShareComponent {
     return null;
   }
 
-  openLink(event: MouseEvent): void {
+  public onClick(): void {
     this.bottomSheetRef.dismiss();
-    event.preventDefault();
   }
 
   public transformedLinks: ProviderInfo[];
@@ -51,15 +54,43 @@ export class ShareComponent {
     //"https://api.whatsapp.com/send?text=[postTitle] [postUrl]",
     //"https://delicious.com/save?v=5&provider=[provider]&noui&jump=close&url=[postUrl]&title=[postTitle]",
 
-    { url: 'https://www.facebook.com/sharer.php?u=[postUrl]', name: 'Facebook' },
-    { url: 'https://twitter.com/share?url=[postUrl]&text=[postTitle]', name: 'Twitter' },//tested
+    {
+      url: 'https://www.facebook.com/sharer.php?u=[postUrl]',
+      name: 'Facebook',
+      description: 'Only the web address of the Playlists history'
+    },
+    {
+      url: 'https://twitter.com/share?url=[postUrl]&text=[postTitle]',
+      name: 'Twitter',
+      description: 'The web address and page title'
+    },//tested
     //{ url: 'https://plus.google.com/share?url=[postUrl]', name: 'Google' },
-    { url: 'https://pinterest.com/pin/create/bookmarklet/?media=[postImg]&url=[postUrl]&is_video=[isVideo]&description=[postTitle]', name: 'Pinterest' }, // image= 1500/1000
-    { url: 'https://www.linkedin.com/shareArticle?url=[postUrl]&title=[postTitle]', name: 'Linkedin' },
+    {
+      url: 'https://pinterest.com/pin/create/bookmarklet/?media=[postImg]&url=[postUrl]&is_video=[isVideo]&description=[postTitle]',
+      name: 'Pinterest',
+      description: 'The web address, image representation and page title'
+    }, // image= 1500/1000
+    {
+      url: 'https://www.linkedin.com/shareArticle?url=[postUrl]&title=[postTitle]',
+      name: 'Linkedin',
+      description: 'The web address and page title',
+    },// tested },
     //{ url: 'https://bufferapp.com/add?text=[postTitle]&url=[postUrl]', name: 'Bufferapp' },
-    { url: 'https://www.tumblr.com/share/link?url=[postUrl]&name=[postTitle]&description=[postDesc]', name: 'Tumblr' },
-    { url: 'https://reddit.com/submit?url=[postUrl]&title=[postTitle]', name: 'Reddit' },// tested
-    { url: "https://telegram.me/share/url?url=[postUrl]&text=[postTitle]", name: 'Telegram' }
+    {
+      url: 'https://www.tumblr.com/share/link?url=[postUrl]&name=[postTitle]',
+      name: 'Tumblr',
+      description: 'The web address and page title'
+    },// tested },
+    {
+      url: 'https://reddit.com/submit?url=[postUrl]&title=[postTitle]',
+      name: 'Reddit',
+      description: 'The web address and page title'
+    },// tested
+    {
+      url: "https://telegram.me/share/url?url=[postUrl]&text=[postTitle]",
+      name: 'Telegram',
+      description: 'The web address and page title'
+    }
     //{ url: 'https://www.stumbleupon.com/submit?url=[postUrl]&title=[postTitle]', name: 'Stumbleupon' },
     //{ url: 'https://www.evernote.com/clip.action?url=[postUrl]&title=[postTitle]', name: 'Evernote' },
     //{ url: 'https://wordpress.com/press-this.php?u=[postUrl]&t=[postTitle]&s=[postDesc]&i=[postImg]', name: 'Wordpress' },
