91dbaa4eb96b02fdeaf8a65ad4a517c1db1aaed5
user16 <user16@PC3>
2021-05-04 08:54:48 +0300

    pinterest share image

--- spa/src/app/share/provider-info-view-model.ts
+++ spa/src/app/share/provider-info-view-model.ts
@@ -1,6 +1,5 @@
-export class ProviderInfo {
+export class ProviderInfoViewModel {
   public url!: string;
   public name!: string;
   public description!: string;
-  public imageUrl!: string | null;
 }
--- spa/src/app/share/share.component.ts
+++ spa/src/app/share/share.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { UrlTransformer } from "./url-transformer";
 import { ProviderInfo } from "./provider-info";
+import { ProviderInfoViewModel } from "./provider-info-view-model";
 
 @Component({
   selector: 'app-share',
@@ -14,17 +15,18 @@ export class ShareComponent {
 
     const title = this.getOgProperty('title');
     const url = this.getOgProperty('url');
-    const image = this.getOgProperty('image');
     const description = this.getOgProperty('description');
 
-    if (title && url && image && description) {
+    if (title && url && description) {
       this.transformedLinks = this.links.map(item => {
         const urlTransformer = new UrlTransformer(item.url);
 
         urlTransformer.appendParameter('postUrl', url);
         urlTransformer.appendParameter('isVideo', 'false');
         urlTransformer.appendParameter('postTitle', title);
-        urlTransformer.appendParameter('postImg', image);
+        if (item.imageUrl) {
+          urlTransformer.appendParameter('postImg', item.imageUrl);
+        }
         urlTransformer.appendParameter('postDesc', description);
         return {
           name: item.name,
@@ -49,7 +51,7 @@ export class ShareComponent {
     this.bottomSheetRef.dismiss();
   }
 
-  public transformedLinks: ProviderInfo[];
+  public transformedLinks: ProviderInfoViewModel[];
   private links: ProviderInfo[] = [
     //"https://api.whatsapp.com/send?text=[postTitle] [postUrl]",
     //"https://delicious.com/save?v=5&provider=[provider]&noui&jump=close&url=[postUrl]&title=[postTitle]",
@@ -57,39 +59,46 @@ export class ShareComponent {
     {
       url: 'https://www.facebook.com/sharer.php?u=[postUrl]',
       name: 'Facebook',
-      description: 'Only the web address of the Playlists history'
+      description: 'Only the web address of the Playlists history',
+      imageUrl: null
     },
     {
       url: 'https://twitter.com/share?url=[postUrl]&text=[postTitle]',
       name: 'Twitter',
-      description: 'The web address and page title'
+      description: 'The web address and page title',
+      imageUrl: null
     },//tested
     //{ url: 'https://plus.google.com/share?url=[postUrl]', name: 'Google' },
     {
       url: 'https://pinterest.com/pin/create/bookmarklet/?media=[postImg]&url=[postUrl]&is_video=[isVideo]&description=[postTitle]',
       name: 'Pinterest',
-      description: 'The web address, image representation and page title'
+      description: 'The web address, image representation and page title',
+      imageUrl: 'https://playlistshistory.com/assets/social/preview-pinterest.png'
     }, // image= 1500/1000
     {
       url: 'https://www.linkedin.com/shareArticle?url=[postUrl]&title=[postTitle]',
       name: 'Linkedin',
       description: 'The web address and page title',
+      imageUrl: null,
     },// tested },
     //{ url: 'https://bufferapp.com/add?text=[postTitle]&url=[postUrl]', name: 'Bufferapp' },
     {
       url: 'https://www.tumblr.com/share/link?url=[postUrl]&name=[postTitle]',
       name: 'Tumblr',
-      description: 'The web address and page title'
+      description: 'The web address and page title',
+      imageUrl: null
     },// tested },
     {
       url: 'https://reddit.com/submit?url=[postUrl]&title=[postTitle]',
       name: 'Reddit',
-      description: 'The web address and page title'
+      description: 'The web address and page title',
+      imageUrl: null
     },// tested
     {
       url: "https://telegram.me/share/url?url=[postUrl]&text=[postTitle]",
       name: 'Telegram',
-      description: 'The web address and page title'
+      description: 'The web address and page title',
+      imageUrl: null
     }
     //{ url: 'https://www.stumbleupon.com/submit?url=[postUrl]&title=[postTitle]', name: 'Stumbleupon' },
     //{ url: 'https://www.evernote.com/clip.action?url=[postUrl]&title=[postTitle]', name: 'Evernote' },
